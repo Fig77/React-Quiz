756264b0fb0af1381413c4e1311cc780d7645483
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-07-17 20:49:39 -0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App = () =>  {
+  const startTrivia = async () => {};
+  //const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+  const nextQuestion = () =>  {}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Quizz</h1>
+      <button className='start-button' onClick={startTrivia}>Start</button>
     </div>
   );
 }
 
 export default App;
+// bg from Bob Canning
